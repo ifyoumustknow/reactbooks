259d7eccc2c616cb001fe17e2806ed5869e8c1ed
Rob Tang <ifyoumustknow@gmail.com>
2020-02-17 18:48:43 -0800

    jkljal;dsaj

---
+++ movies-app/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const apiPort = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
