ceace7cf0cbabfa86c6ccbd4564776640bf04c35
Rob Tang <ifyoumustknow@gmail.com>
2020-02-22 00:04:05 -0800

    revised search bar

--- src/components/SearchArea/index.js
+++ src/components/SearchArea/index.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import "./style.css";
+import { FaSearch } from 'react-icons/fa';
 
 
 const SearchArea = (props) => {
     return (
         <div>
-            <nav className="navbar   nav_color">
-                <h1 className="navbar-brand" to="/">
-                    GOOGLE Books
-        </h1>
+            <nav className="navbar nav_color">
+                <a href>
+                    <img src="/googlelogo_color_92x30dp.png" width="110px" alt="" />
+                </a>
                 <div>
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <div className="search-area">
-                                <form className="form-inline" onSubmit={props.searchBook} action="">
-                                    <input onChange={props.handleSearch} type="text"
-                                        className="form-control "
+                            <div>
+                                <form onSubmit={props.searchBook} action="">
+                                    <input onKeyDown={props.handleSearch} type="text"
+                                        className="search_bar"
                                         placeholder="Search for Books"
                                         autoComplete="off"
-                                        style={{ width: "300px" }} />
-                                    <button className="btn btn-outline-success my-2 my-sm-0" style={{ margin: "10px", float: "right" }} type="submit">Search</button>
+                                    />
+                                    <div className="search_icon">
+                                        <FaSearch />
+                                    </div>
                                 </form>
                             </div>
                         </li>
                     </ul>
                 </div>
             </nav>
-        </div>
+        </div >
     )
 }
 
