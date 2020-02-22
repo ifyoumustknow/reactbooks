c29dc1a373bb64b4bc14b4962c019e4848d7b574
Rob Tang <ifyoumustknow@gmail.com>
2020-02-22 00:52:11 -0800

    fixed   white-space: nowrap

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -9,7 +9,7 @@ const BookCard = (props) => {
             <a target="_blank" href={props.previewLink} rel="noopener noreferrer">
                 <img className="img_container" src={props.image} alt="" />
                 <div className="desc">
-                    <h2>{props.title}</h2>
+                    <h2>{props.title}</h2 >
                     <h3>{props.author}</h3>
                     <p>Published: {props.published}</p>
                 </div>
